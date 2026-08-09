bc961a1661f9d9a2e71a0fdd993f43778bb58d8f
Rapolu Sai Vaishnav Nandan <sainandan028@gmail.com>
2026-08-09 13:21:51 +0530

    fix: requirements encoding and vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Crisis_AI/',
+  base: '/',
   build: {
-    outDir: '../',
-    emptyOutDir: false,
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 })
