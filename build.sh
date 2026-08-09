f6d0b9b1001ae94f9f0dd064beb305a128a4f981
Rapolu Sai Vaishnav Nandan <sainandan028@gmail.com>
2026-08-09 15:53:38 +0530

    fix: create local virtualenv for render build

--- build.sh
+++ build.sh
@@ -8,5 +8,7 @@ npm install
 npm run build
 cd ..
 
-# Install Python backend dependencies
+# Install Python backend dependencies in a virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
 pip install -r requirements.txt
